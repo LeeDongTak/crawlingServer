058187cb58150e67e97b501186ccb140eb3f1031
LeeDongTak <sunny5796@naver.com>
2023-12-10 21:35:19 +0900

    코드 다시 최적화

--- src/controller.js
+++ src/controller.js
@@ -2,89 +2,77 @@ const puppeteer = require("puppeteer");
 
 exports.crawling = async function (req, res) {
   const { id } = req.params;
-  let data = {};
-  let browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    slowMo: 10,
-  });
-  // let browser = puppeteer.launch({ headless: true,  });
-  let page = (await browser).newPage();
-  await Promise.all([
-    (await page).goto(`https://place.map.kakao.com/${id}`),
-    (await page).waitForNavigation(),
-    (await page).setRequestInterception(true),
-    (
-      await page
-    ).on("request", (req) => {
-      if (
-        req.resourceType() === "stylesheet" ||
-        req.resourceType() === "font" ||
-        req.resourceType() === "image"
-      ) {
-        req.abort();
-      } else {
-        req.continue();
-      }
-    }),
-  ]);
+  const data = {};
+  let browser;
 
-  //   대표이미지
-  let bannerImgEh = (await page).$("div.details_present > a.link_present");
-  let bannerImg = (await bannerImgEh).$eval("span.bg_present", (el) => {
-    return el.style.backgroundImage;
-  });
+  try {
+    // Puppeteer 브라우저 실행
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      slowMo: 10,
+    });
 
-  //   평점
-  let StarPointEh = (await page).$(
-    "div.location_evaluation > a.link_evaluation"
-  );
-  let StarPoint = (await StarPointEh).$eval("span.color_b", (el) => {
-    return el.textContent;
-  });
+    // 새로운 페이지 생성
+    const page = await browser.newPage();
 
-  //   영업시간
-  let businessHoursEh = (await page).$(
-    "div.placeinfo_default > div.location_detail > div.location_present > div.displayPeriodList > ul.list_operation"
-  );
-  let businessHours = (await businessHoursEh).$eval(
-    "li > span.txt_operation",
-    (el) => {
-      return el.textContent;
+    try {
+      // 페이지 이동 및 대기 시간 최적화
+      console.log("Debug: Starting page navigation");
+      await page.goto(`https://place.map.kakao.com/${id}`, { waitUntil: 'domcontentloaded' });
+      console.log("Debug: Page navigation successful");
+
+      // 요소 선택 및 데이터 추출 병렬 처리
+      const [bannerImg, StarPoint, businessHours, menuData] = await Promise.all([
+        page.$eval('div.details_present > a.link_present span.bg_present', (el) => el.style.backgroundImage),
+        page.$eval('div.location_evaluation > a.link_evaluation span.color_b', (el) => el.textContent),
+        page.$eval('div.placeinfo_default > div.location_detail > div.location_present > div.displayPeriodList > ul.list_operation li > span.txt_operation', (el) => el.textContent),
+        page.$$eval('ul.list_menu > li[data-page="1"]', (elements) => {
+          return elements.map((el) => ({
+            menuTitle: el.querySelector('span.loss_word').textContent,
+            menuPrice: el.querySelector('em.price_menu').textContent,
+          }));
+        }),
+      ]);
+
+      // 데이터 저장
+      data.bannerImg = bannerImg;
+      data.StarPoint = StarPoint;
+      data.businessHours = businessHours;
+      data.menuData = menuData;
+    } catch (error) {
+      // 디버깅을 위한 에러 출력
+      console.error("Error during page navigation or data extraction:", error.message);
+      throw error;
+    } finally {
+      // 페이지 닫기
+      await page.close();
     }
-  );
 
-  //   매뉴
-  let menuData = [];
-  let menuEh = (await page).$$("ul.list_menu > li[data-page='1']");
-  for (eh of await menuEh) {
-    let menuTitle = (await eh).$eval("span.loss_word", (el) => {
-      return el.textContent;
+    // 브라우저 닫기
+    await browser.close();
+
+    if (!Object.keys(data).length) {
+      return res.send({
+        isSuccess: false,
+        code: 400,
+        message: "크롤링 실패 관리자에게 문의하세요",
+      });
+    }
+
+    return res.send({
+      result: data,
+      isSuccess: true,
+      code: 200,
+      message: "크롤링 성공",
     });
-    let menuPrice = (await eh).$eval("em.price_menu", (el) => {
-      return el.textContent;
-    });
-    menuData.push({ menuTitle: await menuTitle, menuPrice: await menuPrice });
-  }
-
-  data.bannerImg = await bannerImg;
-  data.StarPoint = await StarPoint;
-  data.businessHours = await businessHours;
-  data.menuData = menuData;
-
-  (await browser).close();
-
-  if (!data) {
-    return await res.send({
+  } catch (error) {
+    // 브라우저 닫기 및 에러 응답
+    console.error("Error occurred:", error.message);
+    if (browser) await browser.close();
+    return res.send({
       isSuccess: false,
-      code: 400,
-      message: "크롤링 실패 관리자에게 문의하세요",
+      code: 500,
+      message: "서버 오류"+error,
     });
   }
-
-  return await res.send({
-    result: data,
-    isSuccess: true,
-    code: 200,
-    message: "크롤링 성공",
-  });
 };
