6e861e25f45ec05c579cc22f52b356a7caf56474
LeeDongTak <sunny5796@naver.com>
2023-12-10 17:12:57 +0900

    크롤링 코드 최적화

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {router} = require("./src/router.js")
 
 const express = require('express');
 const app = express()
-const port = 3000
+const port = 8080
 
 // express 미들웨어 설정
 
--- src/controller.js
+++ src/controller.js
@@ -2,89 +2,77 @@ const puppeteer = require("puppeteer");
 
 exports.crawling = async function (req, res) {
   const { id } = req.params;
-  let data = {};
-  let browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    slowMo: 10,
-  });
-  // let browser = puppeteer.launch({ headless: true,  });
-  let page = (await browser).newPage();
-  await Promise.all([
-    (await page).goto(`https://place.map.kakao.com/${id}`),
-    (await page).waitForNavigation(),
-    (await page).setRequestInterception(true),
-    (
-      await page
-    ).on("request", (req) => {
-      if (
-        req.resourceType() === "stylesheet" ||
-        req.resourceType() === "font" ||
-        req.resourceType() === "image"
-      ) {
+  const data = {};
+  let browser;
+
+  try {
+    browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      slowMo: 10,
+    });
+
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+
+    page.on("request", (req) => {
+      if (["stylesheet", "font", "image"].includes(req.resourceType())) {
         req.abort();
       } else {
         req.continue();
       }
-    }),
-  ]);
+    });
 
-  //   대표이미지
-  let bannerImgEh = (await page).$("div.details_present > a.link_present");
-  let bannerImg = (await bannerImgEh).$eval("span.bg_present", (el) => {
-    return el.style.backgroundImage;
-  });
+    await page.goto(`https://place.map.kakao.com/${id}`);
+    
+    // Extracting data...
+    data.bannerImg = await page.$eval(
+      "div.details_present > a.link_present span.bg_present",
+      (el) => el.style.backgroundImage
+    );
 
-  //   평점
-  let StarPointEh = (await page).$(
-    "div.location_evaluation > a.link_evaluation"
-  );
-  let StarPoint = (await StarPointEh).$eval("span.color_b", (el) => {
-    return el.textContent;
-  });
+    data.StarPoint = await page.$eval(
+      "div.location_evaluation > a.link_evaluation span.color_b",
+      (el) => el.textContent
+    );
 
-  //   영업시간
-  let businessHoursEh = (await page).$(
-    "div.placeinfo_default > div.location_detail > div.location_present > div.displayPeriodList > ul.list_operation"
-  );
-  let businessHours = (await businessHoursEh).$eval(
-    "li > span.txt_operation",
-    (el) => {
-      return el.textContent;
+    data.businessHours = await page.$eval(
+      "div.placeinfo_default > div.location_detail > div.location_present > div.displayPeriodList > ul.list_operation li > span.txt_operation",
+      (el) => el.textContent
+    );
+
+    data.menuData = await page.$$eval(
+      "ul.list_menu > li[data-page='1']",
+      (elements) => {
+        return elements.map((el) => ({
+          menuTitle: el.querySelector("span.loss_word").textContent,
+          menuPrice: el.querySelector("em.price_menu").textContent,
+        }));
+      }
+    );
+
+    await browser.close();
+
+    if (Object.keys(data).length === 0) {
+      return res.send({
+        isSuccess: false,
+        code: 400,
+        message: "크롤링 실패 관리자에게 문의하세요",
+      });
     }
-  );
 
-  //   매뉴
-  let menuData = [];
-  let menuEh = (await page).$$("ul.list_menu > li[data-page='1']");
-  for (eh of await menuEh) {
-    let menuTitle = (await eh).$eval("span.loss_word", (el) => {
-      return el.textContent;
+    return res.send({
+      result: data,
+      isSuccess: true,
+      code: 200,
+      message: "크롤링 성공",
     });
-    let menuPrice = (await eh).$eval("em.price_menu", (el) => {
-      return el.textContent;
-    });
-    menuData.push({ menuTitle: await menuTitle, menuPrice: await menuPrice });
-  }
-
-  data.bannerImg = await bannerImg;
-  data.StarPoint = await StarPoint;
-  data.businessHours = await businessHours;
-  data.menuData = menuData;
-
-  (await browser).close();
-
-  if (!data) {
-    return await res.send({
+  } catch (error) {
+    console.error("웹 스크래핑 중 오류:", error.message);
+    if (browser) await browser.close();
+    return res.send({
       isSuccess: false,
-      code: 400,
-      message: "크롤링 실패 관리자에게 문의하세요",
+      code: 500,
+      message: "서버 오류",
     });
   }
-
-  return await res.send({
-    result: data,
-    isSuccess: true,
-    code: 200,
-    message: "크롤링 성공",
-  });
 };
